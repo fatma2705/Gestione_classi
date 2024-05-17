f2734566779d094d7d395c084b134e12276fb0a2
fatma2705 <arafatma2003@gmail.com>
2024-05-17 10:44:57 +0200

    interfaccie StudenteRepository & ClasseRepository aggiunte

---
+++ src/main/java/it/prova/gestioneclasse/repository/ClasseRepository.java
@@ -0,0 +1,9 @@
+package it.prova.gestioneclasse.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.gestioneclasse.model.Classe;
+
+public interface ClasseRepository extends CrudRepository<Classe,Long>{
+
+}
---
+++ src/main/java/it/prova/gestioneclasse/repository/StudenteRepository.java
@@ -0,0 +1,9 @@
+package it.prova.gestioneclasse.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.gestioneclasse.model.Studente;
+
+public interface StudenteRepository extends CrudRepository<Studente,Long>{
+
+}
